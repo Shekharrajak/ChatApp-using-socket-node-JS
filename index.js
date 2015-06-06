de6475ca91a8731e401de9d7f902eefab5451f08
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-07 01:04:30 +0530

    index added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+var io = require('socket.io')(http);
 
 http.listen(3000,function(){
 	console.log('listening on *:3000');
